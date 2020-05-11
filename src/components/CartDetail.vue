f1ec99c6eb55329bd064ee791825ff4d5ae2d427
Th3Cod3 <th3-cod3-1230@hotmail.com>
2020-05-11 12:00:29 -0400

    extra info on print layout

--- src/components/CartDetail.vue
+++ src/components/CartDetail.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="row">
-    <div class="col">
-      {{ product.product }}
+    <div class="col-lg-7 col-md-6">{{ product.product }}</div>
+    <div class="col-1 text-right d-none d-print-flex">
+      {{ product.discount }}%
     </div>
-    <div
-      :class="{
-        'col-2': changeAmount,
-        'col-4': !changeAmount,
-        'text-right': true
-      }"
-    >
-      <span v-show="!changeAmount" @click="changeAmountToggle">
-        {{ product.amount }}
-      </span>
+    <div class="col-2 text-right d-none d-print-flex">
+      {{ -((product.discount / 100) * product.price) | money }}
+    </div>
+    <div class="col-lg-2 col-md-1 text-right">
+      <span v-show="!changeAmount" @click="changeAmountToggle">{{
+        product.amount
+      }}</span>
       <span v-show="changeAmount">
         <div class="d-print-none">
           <input
@@ -24,9 +22,7 @@
         </div>
       </span>
     </div>
-    <div class="col-3 text-right">
-      {{ product.price | money }}
-    </div>
+    <div class="col-lg-3 col-md-2 text-right">{{ product.price | money }}</div>
   </div>
 </template>
 
